45a55c48fb33348959ea0d75cafc86182974200e
bassy1992 <wyarquah@gmail.com>
2025-10-28 17:20:40 +0000

    Verify featured tours without prices

--- Tfront/client/pages/Index.tsx
+++ Tfront/client/pages/Index.tsx
@@ -523,7 +523,7 @@ export default function Index() {
         </div>
       </section>
 
-      {/* Featured Tours */}
+      {/* Featured Tours - No Prices Displayed */}
       <section className="py-16 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center space-y-4 mb-12">
